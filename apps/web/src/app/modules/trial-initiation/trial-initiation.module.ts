7615d5e2bcfeb656d2d33d6022abc63335dfc7ef
rr-bw <rrichetto@bitwarden.com>
2022-08-18 12:42:41 -0700

    remove comments and families layout

--- apps/web/src/app/modules/trial-initiation/trial-initiation.module.ts
+++ apps/web/src/app/modules/trial-initiation/trial-initiation.module.ts
@@ -13,7 +13,6 @@ import { DefaultContentComponent } from "./default-content.component";
 import { EnterpriseContentComponent } from "./enterprise-content.component";
 import { Enterprise1ContentComponent } from "./enterprise1-content.component";
 import { Enterprise2ContentComponent } from "./enterprise2-content.component";
-import { FamiliesContentComponent } from "./families-content.component";
 import { LogoCnetComponent } from "./logo-cnet.component";
 import { LogoForbesComponent } from "./logo-forbes.component";
 import { LogoUSNewsComponent } from "./logo-us-news.component";
@@ -30,7 +29,6 @@ import { TrialInitiationComponent } from "./trial-initiation.component";
     EnterpriseContentComponent,
     Enterprise1ContentComponent,
     Enterprise2ContentComponent,
-    FamiliesContentComponent,
     TeamsContentComponent,
     Teams1ContentComponent,
     Teams2ContentComponent,
@@ -44,36 +42,3 @@ import { TrialInitiationComponent } from "./trial-initiation.component";
   exports: [TrialInitiationComponent],
 })
 export class TrialInitiationModule {}
-
-// import { CnetEnterpriseContentComponent } from "./cnet-enterprise-content.component";
-// import { CnetIndividualContentComponent } from "./cnet-individual-content.component";
-// import { CnetTeamsContentComponent } from "./cnet-teams-content.component";
-// import { DefaultContentComponent } from "./default-content.component";
-// import { EnterpriseContentComponent } from "./enterprise-content.component";
-// import { Enterprise1ContentComponent } from "./enterprise1-content.component";
-// import { Enterprise2ContentComponent } from "./enterprise2-content.component";
-// import { FamiliesContentComponent } from "./families-content.component";
-// import { TeamsContentComponent } from "./teams-content.component";
-// import { Teams1ContentComponent } from "./teams1-content.component";
-// import { Teams2ContentComponent } from "./teams2-content.component";
-// import { TrialInitiationComponent } from "./trial-initiation.component";
-
-// @NgModule({
-//   imports: [SharedModule, CdkStepperModule, VerticalStepperModule, FormFieldModule],
-//   declarations: [
-//     TrialInitiationComponent,
-//     CnetEnterpriseContentComponent,
-//     CnetIndividualContentComponent,
-//     CnetTeamsContentComponent,
-//     DefaultContentComponent,
-//     EnterpriseContentComponent,
-//     Enterprise1ContentComponent,
-//     Enterprise2ContentComponent,
-//     FamiliesContentComponent,
-//     TeamsContentComponent,
-//     Teams1ContentComponent,
-//     Teams2ContentComponent,
-//   ],
-//   exports: [TrialInitiationComponent],
-// })
-// export class TrialInitiationModule {}
